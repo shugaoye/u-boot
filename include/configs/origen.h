66f4a3644d0df3f96b26263e854cb4c47f30ea7d
Angus Ainslie <angus.ainslie@linaro.org>
2011-08-22 11:38:12 -0600

    Add a default env that will use boot.scr from the vfat partition
    Signed-off-by: Angus Ainslie <angus.ainslie@linaro.org>

--- include/configs/origen.h
+++ include/configs/origen.h
@@ -96,7 +96,32 @@
 #define CONFIG_SPL
 #define COPY_BL2_FNPTR_ADDR	0x02020030
 
-#define CONFIG_BOOTCOMMAND	"fatload mmc 0 40007000 uImage; bootm 40007000"
+#define CONFIG_EXTRA_ENV_SETTINGS \
+        "loadaddr=0x42000000\0" \
+        "console=ttySAC2,115200n8\0" \
+        "mmcdev=0:2\0" \
+        "mmcroot=/dev/mmcblk0p2 rw\0" \
+        "rootwait\0" \
+        "mmcargs=setenv bootargs console=${console} " \
+	"root=${mmcroot}\0" \
+        "loadbootscript=fatload mmc ${mmcdev} ${loadaddr} boot.scr\0" \
+        "bootscript=echo Running bootscript from mmc${mmcdev} ...; " \
+	"source ${loadaddr}\0" \
+        "loaduimage=fatload mmc ${mmcdev} ${loadaddr} uImage\0" \
+        "mmcboot=echo Booting from mmc${mmcdev} ...; " \
+	"run mmcargs; " \
+	"bootm ${loadaddr}\0" \
+
+#define CONFIG_BOOTCOMMAND \
+        "if mmc rescan ${mmcdev}; then " \
+	"if run loadbootscript; then " \
+	"run bootscript; " \
+                "else " \
+	"if run loaduimage; then " \
+	"run mmcboot; " \
+	"fi; " \
+	"fi; " \
+        "fi"
 
 /* Miscellaneous configurable options */
 #define CONFIG_SYS_LONGHELP		/* undef to save memory */
