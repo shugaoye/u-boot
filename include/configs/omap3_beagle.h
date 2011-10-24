772a1d7e8053ecbfbac2abdeffc9e3666284c87c
John Rigby <john.rigby@linaro.org>
2011-10-23 23:42:23 -0600

    OMAP3: Beagle: Enable PXE support
    Signed-off-by: John Rigby <john.rigby@linaro.org>

--- include/configs/omap3_beagle.h
+++ include/configs/omap3_beagle.h
@@ -142,6 +142,18 @@
 #define CONFIG_USB_ETHER_SMSC95XX
 #define CONFIG_USB_ETHER_ASIX
 
+/* BOOTP options */
+#define CONFIG_BOOTP_BOOTFILESIZE
+#define CONFIG_BOOTP_BOOTPATH
+#define CONFIG_BOOTP_GATEWAY
+#define CONFIG_BOOTP_HOSTNAME
+
+/* PXE */
+#define CONFIG_CMD_PXE
+#define CONFIG_MENU
+#define CONFIG_BOOTP_PXE
+#define CONFIG_BOOTP_PXE_CLIENTARCH	0x100
+#define CONFIG_BOOTP_VCI_STRING		"U-boot.armv7.omap3_beagle"
 
 /* commands to include */
 #include <config_cmd_default.h>
@@ -310,6 +322,12 @@
 		"if run loaduimage; then " \
 			"run mmcboot;" \
 		"fi;" \
+	"fi;" \
+	"if usb start; then " \
+		"setenv autoload no; "\
+		"bootp; "\
+		"pxe get;" \
+		"pxe boot;" \
 	"fi;" \
 	"run nandboot;" \
 
